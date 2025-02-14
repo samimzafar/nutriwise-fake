5fc4910edd4be58a042f8b9c1056990aac4ad0b4
Samim Zafar <47691227+samimzafar@users.noreply.github.com>
2025-02-14 11:15:47 +0500

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://3.133.147.196/'
+// export const BASE_URL = 'http://3.133.147.196/'
+export const BASE_URL = "/api";
 export const LOCAL_URL = 'http://localhost:3000/'
 export const END_POINTS = {
   ADMIN: {
